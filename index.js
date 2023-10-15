3233cf4d34031a52f7a73598b515ae394ba70b3d
BraxtonMedeiros <97545999+BraxtonMedeiros@users.noreply.github.com>
2023-10-14 23:49:03 -0600

    Added correct path

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app
-  .use(bodyParser.json())
   .use((req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
     );
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
+  .use(bodyParser.json())
   .use('/', require('./routes'));
 
 mongodb.initDb((err) => {
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/fantasy', require('./fantasy'));
+router.use('/', require('./swagger'));
 
 module.exports = router;
