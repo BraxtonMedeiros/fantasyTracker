99e5df674d03d925c07e52977c8e2b6578d7ec98
BraxtonMedeiros <97545999+BraxtonMedeiros@users.noreply.github.com>
2023-10-28 01:16:45 -0600

    Added AUTH0

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,31 @@
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SESSION_SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+};
+
 const express = require('express');
 const router = express.Router();
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+router.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+router.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+router.get('/fantasy', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
 
 router.use('/fantasy', require('./fantasy'));
 router.use('/', require('./swagger'));
